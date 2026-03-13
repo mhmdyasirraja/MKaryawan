449d88d94e8f4411fdf7e0e28cbd6658bad46933
mhmdyasirraja <mhmdyasir00@gmail.com>
2026-03-13 17:01:57 +0700

    Praktikum 3 MVC: tambah ListBarangController, view list_barang, dan route listbarang

--- routes/web.php
+++ routes/web.php
@@ -1,11 +1,32 @@
 <?php
 
-use App\Http\Controllers\HomeController; 
+use App\Http\Controllers\HomeController;
+use App\Http\Controllers\ListBarangController;
 use Illuminate\Support\Facades\Route;
 
 //Route::get('/', function () {
-    //return view('welcome');
+//return view('welcome');
 //});
 
-Route::get('/', [HomeController::class, 'index']); 
-Route::get('/contact', [HomeController::class, 'contact']); 
+Route::get('/', [HomeController::class, 'index']);
+Route::get('/contact', [HomeController::class, 'contact']);
+Route::get('/welcome', function () {
+    return view('welcome');
+});
+Route::get('/user/{id}', function ($id) {
+    return 'user dengan id ' . $id;
+});
+Route::prefix('admin')->group(function () {
+    route::get('/dashboard', function () {
+        return 'admin dashboard';
+    });
+
+    Route::get('/users', function () {
+        return 'admin users';
+    });
+});
+//Route::get('/listbarang/{id}/{$nama}', function ($id, $nama) {
+//return view('list_barang', compact('id', 'nama'));
+//});
+
+Route::get('listbarang/{id}/{nama}', [ListBarangController::class, 'tampilkan']);
